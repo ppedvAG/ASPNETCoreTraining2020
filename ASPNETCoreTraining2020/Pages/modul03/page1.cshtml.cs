8eb93cbcb2e70c411a01c952eeb9b181872d0dcb
Hannes Preishuber <hannesp@ppedv.de>
2020-12-15 12:05:29 +0100

    Razor Pages sample

---
+++ ASPNETCoreTraining2020/Pages/modul03/page1.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETCoreTraining2020.Pages.modul03
+{
+    public class page1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- ASPNETCoreTraining2020/Pages/modul03/page2.cshtml.cs
+++ ASPNETCoreTraining2020/Pages/modul03/page2.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ASPNETCoreTraining2020.modul03
+namespace ASPNETCoreTraining2020.Pages.modul03
 {
-    public class page1Model : PageModel
+    public class page2Model : PageModel
     {
         public void OnGet()
         {
