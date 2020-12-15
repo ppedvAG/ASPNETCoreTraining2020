867fb4d0ccef110dd403ec7499d988d7be6f4a7f
Hannes Preishuber <hannesp@ppedv.de>
2020-12-15 13:17:50 +0100

    Razor Pages

--- ASPNETCoreTraining2020/Pages/modul03/page2.cshtml.cs
+++ ASPNETCoreTraining2020/Pages/modul03/page2.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNETCoreTraining2020.modul02;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,19 @@ namespace ASPNETCoreTraining2020.Pages.modul03
 {
     public class page2Model : PageModel
     {
+        Class1 class1;
+        public page2Model(Class1 _class1)
+        {
+            class1 = _class1;
+        }
+        public int MyProperty { get; set; }
         public void OnGet()
         {
+        }
+        public string Datum()
+        {
+            return DateTime.Now.ToShortDateString();
+
         }
     }
 }
