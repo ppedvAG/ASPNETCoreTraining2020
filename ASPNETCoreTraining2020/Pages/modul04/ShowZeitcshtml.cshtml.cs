e98535ee02be21e9cfb8d784ca9c767bd13a733e
Hannes Preishuber <hannesp@ppedv.de>
2020-12-15 16:01:52 +0100

    Einfache Partial Page

---
+++ ASPNETCoreTraining2020/Pages/modul04/ShowZeitcshtml.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETCoreTraining2020.Pages.modul04
+{
+    public class ShowZeitcshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
