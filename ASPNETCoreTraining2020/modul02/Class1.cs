a25cd122f5ea88c1f661430d97a0fa851ed3f4c7
Hannes Preishuber <hannesp@ppedv.de>
2020-12-15 11:30:21 +0100

    Projektdateien hinzufügen.

---
+++ ASPNETCoreTraining2020/modul02/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreTraining2020.modul02
+{
+    public class Class1
+    {
+    }
+}
