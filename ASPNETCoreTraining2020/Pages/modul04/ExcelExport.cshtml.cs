c4c4d2c79c6e4ebaa91da5ccccbef2b3d700e7c0
Hannes Preishuber <hannesp@ppedv.de>
2020-12-15 16:20:34 +0100

    Mini Service Excel Export mit Partial View

---
+++ ASPNETCoreTraining2020/Pages/modul04/ExcelExport.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNETCoreTraining2020.Pages.modul04
+{
+    public class ExcelExportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public PartialViewResult OnGetExport()
+        {
+            HttpContext.Response.ContentType = "application/vnd.ms-excel";
+            return Partial("_excel");
+        }
+    }
+}
