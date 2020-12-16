c9da3556d6fc1334d03beb4c5dfacd91e1cc700c
Hannes Preishuber <hannesp@ppedv.de>
2020-12-16 13:25:53 +0100

    Todo Model Klasse aus https://jsonplaceholder.typicode.com/todos

---
+++ ASPNETCoreTraining2020/Pages/Modul07/Aufgabe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreTraining2020.Pages.Modul07
+{
+
+ 
+
+    public class Aufgabe
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
