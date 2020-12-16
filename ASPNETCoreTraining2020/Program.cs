16aa27f7515a534255b0525d42b8be1869db44cd
Hannes Preishuber <hannesp@ppedv.de>
2020-12-16 09:59:32 +0100

    Serilog Console und File

--- ASPNETCoreTraining2020/Program.cs
+++ ASPNETCoreTraining2020/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,14 @@ namespace ASPNETCoreTraining2020
     {
         public static void Main(string[] args)
         {
+
+            Log.Logger = new LoggerConfiguration()
+         .MinimumLevel.Debug()
+      .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+      .Enrich.FromLogContext()
+      .WriteTo.Console()
+      .WriteTo.File(@"c:\temp\logs\log.txt")
+          .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -20,7 +30,10 @@ namespace ASPNETCoreTraining2020
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog();
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+          ;
+
     }
 }
