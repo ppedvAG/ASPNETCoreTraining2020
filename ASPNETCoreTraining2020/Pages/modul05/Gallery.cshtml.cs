e2cc1c80d650611a2d36f4b1b48f9ed8e3ab47a4
Hannes Preishuber <hannesp@ppedv.de>
2020-12-16 10:32:01 +0100

    Image Gallery Versuch 1

--- ASPNETCoreTraining2020/Pages/modul05/Gallery.cshtml.cs
+++ ASPNETCoreTraining2020/Pages/modul05/Gallery.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,11 @@ namespace ASPNETCoreTraining2020.Pages.modul05
 {
     public class GalleryModel : PageModel
     {
+        public string[] BildListe { get; set; }
         public void OnGet()
         {
+            var pfad = AppDomain.CurrentDomain.GetData("ImgDir") + @"\img\" ;
+            BildListe = Directory.GetFiles(pfad);
         }
     }
 }
